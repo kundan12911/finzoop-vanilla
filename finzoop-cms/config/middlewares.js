f68f6c759752ee94d47167199113c3139fe17fb8
kundan12911 <kuntenormin@gmail.com>
2026-04-06 15:24:22 +0530

    feat: add Railway deployment config for Strapi CMS

--- finzoop-cms/config/middlewares.js
+++ finzoop-cms/config/middlewares.js
@@ -17,7 +17,13 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'https://finzoop.com', 'https://www.finzoop.com'],
+      origin: [
+        'http://localhost:3000',
+        'https://finzoop.com',
+        'https://www.finzoop.com',
+        /\.vercel\.app$/,   // all Vercel preview deployments
+        /\.railway\.app$/,  // Railway service domains
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
